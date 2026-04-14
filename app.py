b7716c946fc83902a6bf46957882bbc9a63119a1
quentindubos80270 <quentin.dubos80270@gmail.com>
2026-04-14 17:32:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,10 +20,10 @@ def hello():
 
 def get_db_connection():
         conn = psycopg2.connect(
-            host=DB_HOST
-            port=DB_PORT
-            dbname=DB_NAME
-            user=DB_USER
+            host=DB_HOST,
+            port=DB_PORT,
+            dbname=DB_NAME,
+            user=DB_USER,
             password=DB_PASSWORD
         )
         return conn
